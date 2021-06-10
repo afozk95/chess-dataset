c0f08b94a64fa446acfa357d530e3ba4c27ac784
afozk95 <afozk95@gmail.com>
2021-06-10 20:31:04 +0300

    new features for serializer

---
+++ model.py
@@ -0,0 +1,114 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from torch import optim
+
+
+class MyDataset(Dataset):
+    def __init__(self):
+        dat = np.load("dataset.npz")
+        self.X = dat["X"] 
+        self.y = torch.tensor([self._parse_y(v)+3 for v in dat["y"].tolist()])
+
+    def _parse_y(self, val):
+        if val == 100:
+            return 3
+        elif val == -100:
+            return -3
+        elif val > 3.0:
+            return 2
+        elif val > 1.0:
+            return 1
+        elif val > -1.0:
+            return 0
+        elif val > -3.0:
+            return -1
+        else:
+            return -2
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.a1 = nn.Conv2d(19, 64, kernel_size=3, padding=1)
+        self.a2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.a3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+
+        self.b1 = nn.Conv2d(128, 128, kernel_size=3, padding=0)
+        self.b2 = nn.Conv2d(128, 128, kernel_size=3, padding=0)
+        self.b3 = nn.Conv2d(128, 128, kernel_size=4, padding=0)
+
+        self.linear = nn.Linear(128, 7)
+
+    def forward(self, x):
+        x = F.relu(self.a1(x))
+        x = F.relu(self.a2(x))
+        x = F.relu(self.a3(x))
+
+        x = F.relu(self.b1(x))
+        x = F.relu(self.b2(x))
+        x = F.relu(self.b3(x))
+
+        x = x.view(-1, 128)
+        x = self.linear(x)
+
+        return x
+
+
+if __name__ == "__main__":
+    device = "cuda"
+
+    chess_dataset = MyDataset()
+    train_ratio = 0.8
+    train_length = int(len(chess_dataset) * train_ratio)
+    test_length = len(chess_dataset) - train_length
+    train_dataset, test_dataset = torch.utils.data.random_split(chess_dataset, (train_length, test_length))
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=4096, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=4096, shuffle=True)
+    model = Net()
+    optimizer = optim.Adam(model.parameters())
+    floss = nn.CrossEntropyLoss(reduction="sum")
+
+    if device == "cuda":
+        model.cuda()
+
+    model.train()
+
+    for epoch in range(1, 101):
+        epoch_loss = 0
+        epoch_correct = 0
+        num_examples_epoch = 0
+        print(f"Epoch {epoch}")
+        for batch_idx, (data, target) in enumerate(train_loader, start=1):
+            print(f"Batch {batch_idx}")
+            data, target = data.to(device), target.to(device)
+            data = data.float()
+
+            optimizer.zero_grad()
+            output = model(data)
+
+            loss = floss(output, target)
+            loss.backward()
+            optimizer.step()
+            
+            batch_loss = loss.item()
+            batch_correct = (output.max(1).indices == target).sum().item()
+            num_examples_batch = target.shape[0]
+            epoch_loss += batch_loss
+            epoch_correct += batch_correct
+            num_examples_epoch += num_examples_batch
+            
+            print(f"loss in epoch: {epoch_loss/num_examples_epoch}")
+            print(f"accuracy in epoch: {epoch_correct/num_examples_epoch}")
+            print(f"loss in batch: {batch_loss/num_examples_batch}")
+            print(f"accuracy in batch: {batch_correct/num_examples_batch}")
+
+        torch.save(model.state_dict(), f"nets/value_{epoch}.pth")
--- serializer.py
+++ serializer.py
@@ -1,8 +1,9 @@
+from typing import Union
 import chess
 import numpy as np
 
 
-class BoardSerializer:
+class ChessPositionSerializer:
     def __init__(
         self,
         is_pieces: bool = True,
@@ -26,67 +27,175 @@ class BoardSerializer:
         )
         return PIECE_TYPE_TO_INT[piece_type]
 
-    def _serialize_pieces(self, board: chess.Board) -> np.ndarray:
+    def _serialize_board_pieces(self, board: chess.Board) -> np.ndarray:
         if not self.is_pieces:
-            return np.empty(shape=(8, 8, 0), dtype=self.dtype)
+            return np.empty(shape=(0, 8, 8), dtype=self.dtype)
 
-        pieces = np.zeros(shape=(8 * 8, 12), dtype=self.dtype)
+        pieces = np.zeros(shape=(12, 8 * 8), dtype=self.dtype)
         for sq_index, piece_type in board.piece_map().items():
             index = self._piece_type_to_index(piece_type.symbol())
-            pieces[sq_index, index] = 1
+            pieces[index, sq_index] = 1
 
-        return pieces.reshape((8, 8, 12))
+        return pieces.reshape((12, 8, 8))
 
-    def _serialize_enpassant(self, board: chess.Board) -> np.ndarray:
+    def _serialize_board_enpassant(self, board: chess.Board) -> np.ndarray:
         if not self.is_enpassant:
-            return np.empty(shape=(8, 8, 0), dtype=self.dtype)
+            return np.empty(shape=(0, 8, 8), dtype=self.dtype)
 
-        enpassant = np.zeros(shape=(8 * 8, 1), dtype=self.dtype)
+        enpassant = np.zeros(shape=(1, 8 * 8), dtype=self.dtype)
         if board.ep_square:
             sq_index = board.ep_square
             enpassant[sq_index, 0] = 1
 
-        return enpassant.reshape((8, 8, 1))
+        return enpassant.reshape((1, 8, 8))
 
-    def _serialize_castling(self, board: chess.Board) -> np.ndarray:
+    def _serialize_board_castling(self, board: chess.Board) -> np.ndarray:
         if not self.is_castling:
-            return np.empty(shape=(8, 8, 0), dtype=self.dtype)
+            return np.empty(shape=(0, 8, 8), dtype=self.dtype)
 
-        castling = np.zeros(shape=(8, 8, 4), dtype=self.dtype)
-        castling[:, :, 0] = (
+        castling = np.zeros(shape=(4, 8, 8), dtype=self.dtype)
+        castling[0, :, :] = (
             1 if board.has_kingside_castling_rights(color=chess.WHITE) else 0
         )
-        castling[:, :, 1] = (
+        castling[1, :, :] = (
             1 if board.has_queenside_castling_rights(color=chess.WHITE) else 0
         )
-        castling[:, :, 2] = (
+        castling[2, :, :] = (
             1 if board.has_kingside_castling_rights(color=chess.BLACK) else 0
         )
-        castling[:, :, 3] = (
+        castling[3, :, :] = (
             1 if board.has_queenside_castling_rights(color=chess.BLACK) else 0
         )
 
         return castling
 
-    def _serialize_turn(self, board: chess.Board) -> np.ndarray:
+    def _serialize_board_turn(self, board: chess.Board) -> np.ndarray:
         if not self.is_turn:
-            return np.empty(shape=(8, 8, 0), dtype=self.dtype)
+            return np.empty(shape=(0, 8, 8), dtype=self.dtype)
 
-        turn = np.zeros(shape=(8, 8, 2), dtype=self.dtype)
+        turn = np.zeros(shape=(2, 8, 8), dtype=self.dtype)
         if board.turn == chess.WHITE:
-            turn[:, :, 0] = 1
-        else:
-            turn[:, :, 1] = 1
+            turn[0, :, :] = 1
+        elif board.turn == chess.BLACK:
+            turn[1, :, :] = 1
 
         return turn
 
+    def _serialize_fen_pieces(self, fen: str) -> np.ndarray:
+        if not self.is_pieces:
+            return np.empty(shape=(0, 8, 8), dtype=self.dtype)
+
+        pieces_str = fen.split(" ")[0]
+        pieces = np.zeros(shape=(12, 8 * 8), dtype=self.dtype)
+        rank_index = 8
+        file_index = 1
+        for char in pieces_str:
+            if char.isdigit():
+                digit = int(char)
+                file_index += digit
+            elif char.isalpha():
+                index = self._piece_type_to_index(char)
+                sq_index = chess.square(file_index-1, rank_index-1)
+                pieces[index, sq_index] = 1
+                file_index += 1
+            elif char == "/":
+                rank_index -= 1
+                file_index = 1
+
+        return pieces.reshape((12, 8, 8))
+
+    def _serialize_fen_enpassant(self, fen: str) -> np.ndarray:
+        if not self.is_enpassant:
+            return np.empty(shape=(0, 8, 8), dtype=self.dtype)
+
+        enpassant_str = fen.split(" ")[3]
+        enpassant = np.zeros(shape=(1, 8 * 8), dtype=self.dtype)
+        if enpassant_str != "-":
+            sq_index = chess.parse_square(enpassant_str)
+            enpassant[0, sq_index] = 1
+
+        return enpassant.reshape((1, 8, 8))
+
+    def _serialize_fen_castling(self, fen: str) -> np.ndarray:
+        if not self.is_castling:
+            return np.empty(shape=(0, 8, 8), dtype=self.dtype)
+
+        castling_str = fen.split(" ")[2]
+        castling = np.zeros(shape=(4, 8, 8), dtype=self.dtype)
+        castling[0, :, :] = (
+            1 if "K" in castling_str else 0
+        )
+        castling[1, :, :] = (
+            1 if "Q" in castling_str else 0
+        )
+        castling[2, :, :] = (
+            1 if "k" in castling_str else 0
+        )
+        castling[3, :, :] = (
+            1 if "q" in castling_str else 0
+        )
+
+        return castling
+
+    def _serialize_fen_turn(self, fen: str) -> np.ndarray:
+        if not self.is_turn:
+            return np.empty(shape=(0, 8, 8), dtype=self.dtype)
+
+        turn_str = fen.split(" ")[1]
+        turn = np.zeros(shape=(2, 8, 8), dtype=self.dtype)
+        if turn_str == "w":
+            turn[0, :, :] = 1
+        elif turn_str == "b":
+            turn[1, :, :] = 1
+
+        return turn
+
+    def _serialize_board(self, board: chess.Board) -> np.ndarray:
+        pieces = self._serialize_board_pieces(board)
+        enpassant = self._serialize_board_enpassant(board)
+        castling = self._serialize_board_castling(board)
+        turn = self._serialize_board_turn(board)
+        array = np.concatenate([pieces, enpassant, castling, turn], axis=0)
+        return array
+
+    def _serialize_fen(self, fen: str) -> np.ndarray:
+        pieces = self._serialize_fen_pieces(fen)
+        enpassant = self._serialize_fen_enpassant(fen)
+        castling = self._serialize_fen_castling(fen)
+        turn = self._serialize_fen_turn(fen)
+        array = np.concatenate([pieces, enpassant, castling, turn], axis=0)
+        return array
+
     def serialize(
         self,
-        board: chess.Board,
+        position: Union[chess.Board, str],
     ) -> np.ndarray:
-        pieces = self._serialize_pieces(board)
-        enpassant = self._serialize_enpassant(board)
-        castling = self._serialize_castling(board)
-        turn = self._serialize_turn(board)
-        array = np.concatenate([pieces, enpassant, castling, turn], axis=2)
+        if isinstance(position, chess.Board):
+            array = self._serialize_board(position)
+        elif isinstance(position, str):
+            array = self._serialize_fen(position)
+        else:
+            raise TypeError("position must be either str or chess.Board")
+
         return array
+
+
+def parse_eval(y: str) -> float:
+    if y[:2] == "#-":
+        return -100
+    elif y[0] == "#":
+        return 100
+    else:
+        return float(y)
+
+
+if __name__ == "__main__":
+    from dataset import ChessValueDataset
+    import numpy as np
+
+
+    cvd = ChessValueDataset.from_file("cvd.json")
+    s = ChessPositionSerializer()
+    X = np.stack([s.serialize(fen) for fen in cvd.fen_to_value.keys()])
+    y = np.stack([parse_eval(y) for y in cvd.fen_to_value.values()])
+    np.savez("dataset.npz", X=X, y=y)
